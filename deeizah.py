d6eeb5f240800058b689e59c533da9289984ea32
izahzr <izzatiizah8@gmail.com>
2026-06-09 21:39:12 +0800

    Update deeizah.py

--- deeizah.py
+++ deeizah.py
@@ -273,7 +273,7 @@ with tab4:
              use_container_width=True)
 
    
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
 
     # Left Chart: Returns by Category
     ax1.bar(category_returns['Product Category'], category_returns['Return Rate (%)'], color='#E74C3C', edgecolor='black')
